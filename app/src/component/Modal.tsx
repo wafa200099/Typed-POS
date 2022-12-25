b1a8802758c672b35cbb32abbaa3bdf7a75e8f6e
wafa200099 <wafaa.alomour@gmail.com>
2022-12-25 09:55:40 +0200

    Edit modal component

--- app/src/component/Modal.tsx
+++ app/src/component/Modal.tsx
@@ -1,31 +1,22 @@
-import {
-  FC,
-  MutableRefObject,
-  PropsWithChildren,
-  useEffect,
-  useRef,
-} from "react";
-import { createPortal } from "react-dom";
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import * as React from 'react';
 
-const Modal: FC<PropsWithChildren> = ({ children }) => {
-  const elementRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
-  if (!elementRef.current) {
-    elementRef.current = document.createElement("div");
-  }
+type ModalProps = {
+  children?: React.ReactNode;
+};
+
+const Modal: React.FC<ModalProps> = ({ children }) => {
+  const elRef = useRef<HTMLDivElement>(document.createElement('div'));
+
   useEffect(() => {
-    const modalRoot = document.getElementById("modal");
-    if (!modalRoot || !elementRef.current) {
-      return;
-    }
-    modalRoot.append(elementRef.current);
-    return () => {
-      if (elementRef.current) modalRoot.removeChild(elementRef.current);
-    };
+    const modalRoot = document.getElementById('modal');
+    const current = elRef.current;
+    modalRoot!.appendChild(current);
+    return () => void modalRoot!.removeChild(current);
   }, []);
-  return createPortal(
-    <div className="modal">{children} </div>,
-    elementRef.current
-  );
+
+  return createPortal(<div>{children}</div>, elRef.current);
 };
 
 export default Modal;
